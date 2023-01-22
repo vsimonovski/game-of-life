f8f6188aca0432c0a6c1d9e9fc2a6a4f24b6962c
Vladimir Simonovski <vsimonovski@outlook.com>
2023-01-22 13:05:41 +0100

    test: cover cell util methods

---
+++ src/utils/cell.spec.ts
@@ -0,0 +1,65 @@
+import { NEIGHBOUR_CELL_COORDINATES } from 'config';
+import { Cells, GridSize } from 'types';
+import { initialiseCells, countNeighbourCells } from './cell';
+
+describe('initialiseCells util', () => {
+  const mockGridSize: GridSize = { rows: 3, cols: 3 };
+
+  it('should initialise all cells false', () => {
+    const numOfLiveCells = initialiseCells(mockGridSize)
+      .flatMap((cell) => cell)
+      .filter((cell) => cell).length;
+
+    expect(numOfLiveCells).toBe(0);
+  });
+
+  it('should return correct number of cells', () => {
+    const numOfCells = initialiseCells(mockGridSize).flatMap(
+      (cell) => cell
+    ).length;
+    const rectangleGridCells = initialiseCells({ rows: 5, cols: 10 }).flatMap(
+      (cell) => cell
+    );
+    const zeroCells = initialiseCells({ rows: 0, cols: 0 }).flatMap(
+      (cell) => cell
+    );
+
+    expect(numOfCells).toBe(9);
+    expect(rectangleGridCells.length).toBe(50);
+    expect(zeroCells.length).toBe(0);
+  });
+});
+
+describe('countNeighbourCells util', () => {
+  const mockGridSize: GridSize = { rows: 3, cols: 3 };
+  const mockCells = [
+    [false, true, false],
+    [true, false, true],
+    [false, true, false],
+  ];
+  const mockDeadCells = [
+    [false, false, false],
+    [false, false, false],
+    [false, false, false],
+  ];
+
+  it('should return correct number of live neighbours', () => {
+    const neighbourCellCountWrapper = (
+      cells: Cells,
+      rowId: number,
+      colId: number
+    ) =>
+      countNeighbourCells(
+        NEIGHBOUR_CELL_COORDINATES,
+        mockGridSize,
+        cells,
+        rowId,
+        colId
+      );
+
+    expect(neighbourCellCountWrapper(mockCells, 1, 1)).toBe(4);
+    expect(neighbourCellCountWrapper(mockCells, 0, 1)).toBe(3);
+    expect(neighbourCellCountWrapper(mockCells, 1, 0)).toBe(3);
+    expect(neighbourCellCountWrapper(mockDeadCells, 1, 1)).toBe(0);
+  });
+});
